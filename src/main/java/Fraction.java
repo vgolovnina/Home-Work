1a7b79babdd5dbe23c037f748e67303ab80815f6
vgolovnina <dumplingspic@gmail.com>
2024-09-22 17:45:34 +0400

    add Fraction

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -50,9 +50,9 @@ public class Fraction {
 
     public static void main(String[] args) {  // Создание дробей
 
-        Fraction f1 = new Fraction(1, 3); // одна треть
-        Fraction f2 = new Fraction(2, 5); // две пятых
-        Fraction f3 = new Fraction(7, 8); // семь восьмых
+        Fraction f1 = new Fraction(1, 3);
+        Fraction f2 = new Fraction(2, 5);
+        Fraction f3 = new Fraction(7, 8);
 
         Fraction result = f1.sum(f2).sum(f3).minus(5);
         System.out.println(result);
