652d267c6c259335e98f8de8bf6e7f29d2c16fd9
vgolovnina <dumplingspic@gmail.com>
2024-09-22 17:41:51 +0400

    add Fraction

---
+++ src/main/java/Fraction.java
@@ -0,0 +1,61 @@
+public class Fraction {
+    int numerator;
+    int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Знаменатель не может быть равен нулю");
+        }
+        this.denominator = Math.abs(denominator);
+        this.numerator = denominator < 0 ? -numerator : numerator;
+        reduce();
+    }
+
+    void reduce() {
+        int gcd = gcd(Math.abs(numerator), denominator); //наибольший общий делитель
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+
+    public Fraction sum(Fraction other) {
+        int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction sum(int integer) {
+        return this.sum(new Fraction(integer, 1));
+    }
+
+    public Fraction minus(Fraction other) {
+        return this.sum(new Fraction(-other.numerator, other.denominator));
+    }
+
+    public Fraction minus(int integer) {
+        return this.minus(new Fraction(integer, 1));
+    }
+
+    public static void main(String[] args) {  // Создание дробей
+
+        Fraction f1 = new Fraction(1, 3); // одна треть
+        Fraction f2 = new Fraction(2, 5); // две пятых
+        Fraction f3 = new Fraction(7, 8); // семь восьмых
+
+        Fraction result = f1.sum(f2).sum(f3).minus(5);
+        System.out.println(result);
+    }
+
+}
