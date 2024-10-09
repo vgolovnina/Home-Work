ddd41030fa52537d05f49a19ce24d4bd90b7b7e1
vgolovnina <dumplingspic@gmail.com>
2024-10-09 20:28:17 +0400

    change class Point (add class Point3D)

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -12,19 +12,32 @@ public class Point {
     public String toString() {
         return "{" + x + ";" + y + "}";
     }
+}
+
+class Point3D extends Point {
+    int z;
+
+    public Point3D(int x, int y, int z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public String getCoordinates() {
+        return "(" + x + ", " + y + ", " + z + ")";
+    }
 
     public static void main(String[] args) {
-        //создание объектов
-        Point point1 = new Point(1, 3);
-        Point point2 = new Point(1, 3);
-        Point point3 = new Point(5, 8);
+        // создание объектов
+        Point3D point1 = new Point3D(1, 3, 1);
+        Point3D point2 = new Point3D(1, 3, 3);
+        Point3D point3 = new Point3D(5, 8, 6);
 
-        System.out.println("Точка 1: " + point1);
-        System.out.println("Точка 2: " + point2);
-        System.out.println("Точка 3: " + point3);
-        //сравнение объектов в памяти
+        System.out.println("Точка 1: " + point1.getCoordinates());
+        System.out.println("Точка 2: " + point2.getCoordinates());
+        System.out.println("Точка 3: " + point3.getCoordinates());
+
+        // сравнение объектов в памяти
         System.out.println("Точка 1 == Точка 2: " + (point1 == point2));
         System.out.println("Точка 1 == Точка 3: " + (point1 == point3));
     }
-
 }
